28aabf94a60bd7a130ad874dd3b4ec1cae7d57b1
aarav-g-168 <guptaaarav987@gmail.com>
2025-08-28 21:45:23 +0530

    Added the contact us section and improved the UI.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import DomainSection from "@/components/DomainSection";
+import ContactUs from "@/components/ContactUs";
 
 export default function Home() {
   return (
     <>
       <DomainSection />
+      <ContactUs />
     </>
   );
 }
